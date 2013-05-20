1cef7b8a2beb7ab72ead662b5ffeb4198ee22845
Menny Rachmanny <onemenny@gmail.com>
2013-05-20 06:53:07 +0300

    adding mockups views and mockup images

---
+++ ComicTales.Web/Controllers/MockupsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicTales.Web.Controllers
+{
+    public class MockupsController : Controller
+    {
+        //
+        // GET: /Mockups/
+
+        public ActionResult Index()
+        {
+            return ViewComics();
+        }
+
+        public ActionResult ViewComics()
+        {
+            return View("ViewComics");
+        }
+    
+        public ActionResult EditComics()
+        {
+            return View("EditComics");
+        }
+
+        public ActionResult CreateTile()
+        {
+            return View("CreateTile");
+        }
+
+        public ActionResult CreateTileCam()
+        {
+            return View("CreateTileCam");
+        }
+
+        public ActionResult CreateTileUpload()
+        {
+            return View("CreateTileUpload");
+        }
+
+        public ActionResult EditTile()
+        {
+            return View("EditTile");
+        }
+    }
+}
