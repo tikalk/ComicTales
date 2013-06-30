30f333b78d0a8674ee8358de123972f9982dc990
Igor Zelmanovich <igor.israel@gmail.com>
2013-06-30 15:14:31 +0300

    minor

--- ComicTales.Web/Scripts/CreateStory.js
+++ ComicTales.Web/Scripts/CreateStory.js
@@ -5,11 +5,9 @@ var ComicTales;
 (function (ComicTales) {
     function InitStory() {
         var viewModel = new CreateStoryViewModel();
-
         ko.applyBindings(viewModel);
     }
     ComicTales.InitStory = InitStory;
-
     var CreateStoryViewModel = (function () {
         function CreateStoryViewModel() {
             this.storyName = ko.observable('');
@@ -18,14 +16,17 @@ var ComicTales;
             $.ajax({
                 url: 'Story/emptyId/Create',
                 type: 'post',
-                data: { Name: this.storyName() },
-                success: function (data) {
+                data: {
+                    Name: this.storyName()
+                },
+                success: //Name is the ComicStory.cs property
+                function (data) {
                     window.location.href = 'Story/' + data.id + '/Edit/';
                 }
             });
         };
         return CreateStoryViewModel;
     })();
-    ComicTales.CreateStoryViewModel = CreateStoryViewModel;
+    ComicTales.CreateStoryViewModel = CreateStoryViewModel;    
 })(ComicTales || (ComicTales = {}));
 //@ sourceMappingURL=CreateStory.js.map
