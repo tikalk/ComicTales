9258f935a3dcf6254845bfd998bb29adba2e8079
Igor Zelmanovich <igor.israel@gmail.com>
2013-06-30 16:01:46 +0300

    update generated js

--- ComicTales.Web/Scripts/Editor.js
+++ ComicTales.Web/Scripts/Editor.js
@@ -2,13 +2,10 @@ var ComicTales;
 (function (ComicTales) {
     function Init(storyId) {
         var viewModel = new EditorViewModel(storyId);
-
         ko.applyBindings(viewModel);
-
         return viewModel;
     }
     ComicTales.Init = Init;
-
     var EditorViewModel = (function () {
         function EditorViewModel(storyId) {
             this.storyId = storyId;
@@ -17,33 +14,27 @@ var ComicTales;
             this.hasUpdates = ko.observable(false);
             // load data
             this.loadTiles();
-
             // Start the connection
             this.initConnection();
         }
         EditorViewModel.prototype.refresh = function () {
             this.loadTiles();
         };
-
         EditorViewModel.prototype.addNewTile = function () {
             var _this = this;
             this.editTileDialog = new ComicTales.EditTileDialog(function (tile) {
                 _this.saveTile(tile);
             });
-
             this.editTileDialog.open();
         };
-
         EditorViewModel.prototype.editTile = function (tile) {
             this.editTileDialog = new ComicTales.EditTileDialog(function (t) {
             }, tile);
-
             this.editTileDialog.open();
         };
-
         EditorViewModel.prototype.deleteTile = function (tile) {
-        };
-
+            // todo: not implemented yet
+                    };
         EditorViewModel.prototype.saveTile = function (tile) {
             var _this = this;
             $.post('/Story/' + this.storyId + '/AddTile', tile, function () {
@@ -51,18 +42,15 @@ var ComicTales;
                 _this.refresh();
             });
         };
-
         EditorViewModel.prototype.saveStory = function () {
             var _this = this;
             $.post('/Story/' + this.storyId + '/Save', function () {
                 return _this.notifyUpdated();
             });
         };
-
         EditorViewModel.prototype.notifyUpdated = function () {
             $.connection.comicStoryNotificationsHub.server.notifyHasUpdates(this.storyId);
         };
-
         EditorViewModel.prototype.loadTiles = function () {
             var _this = this;
             this.isLoading(true);
@@ -71,7 +59,6 @@ var ComicTales;
                 _this.updateTiles(data);
             });
         };
-
         EditorViewModel.prototype.updateTiles = function (data) {
             var _this = this;
             this.tiles.removeAll();
@@ -79,18 +66,15 @@ var ComicTales;
                 return _this.tiles.push(tile);
             });
         };
-
         EditorViewModel.prototype.initConnection = function () {
             var _this = this;
             // Proxy created on the fly
             var storyNotifications = $.connection.comicStoryNotificationsHub;
-
             // Declare a function on the chat hub so the server can invoke it
             storyNotifications.client.notifyHasUpdates = function () {
                 console.log('Recieve updates notification!');
                 _this.hasUpdates(true);
             };
-
             // Start the connection
             $.connection.hub.start().done(function () {
                 console.log('Now connected, connection ID=' + $.connection.hub.id + ', transport=' + $.connection.hub.transport.name);
@@ -101,6 +85,6 @@ var ComicTales;
         };
         return EditorViewModel;
     })();
-    ComicTales.EditorViewModel = EditorViewModel;
+    ComicTales.EditorViewModel = EditorViewModel;    
 })(ComicTales || (ComicTales = {}));
 //@ sourceMappingURL=Editor.js.map
