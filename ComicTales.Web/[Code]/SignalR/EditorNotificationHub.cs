d008b4c5c3d48d34b78cf97a69a5cb2fcb4edc8b
onemenny <onemenny@gmail.com>
2013-06-30 10:12:58 +0300

    adding missing files

---
+++ ComicTales.Web/[Code]/SignalR/EditorNotificationHub.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace ComicTales
+{
+    [HubName("comicStoryNotificationsHub")]
+    public class StoryNotificationsHub : Hub
+    {
+        // called by the client when editing a story
+        public void Join(string groupId /*actualy this is the story Id*/)
+        {
+            Groups.Add(Context.ConnectionId, groupId);
+        }
+
+        public void SendMessageToGroup(string groupId, string message)
+        {
+            Clients.Group(groupId).addMessageToGroup(message);
+        }
+
+        #region Who Is Connected
+
+        private List<string> _connections = new List<string>();
+
+        //called by a timer
+        public void WhoIsConnected()
+        {
+            var context = GlobalHost.ConnectionManager.GetHubContext<StoryNotificationsHub>();
+            context.Clients.All.areYouConnected();
+            _connections.Clear();
+        }
+
+        public void IAmConnected()
+        {
+            if (!_connections.Contains(Context.ConnectionId))
+            {
+                _connections.Add(Context.ConnectionId);
+            }
+        }
+
+        #endregion
+
+        #region Override
+
+        public static HashSet<string> ClientsMonitor = new HashSet<string>();
+
+        public override System.Threading.Tasks.Task OnConnected()
+        {
+            Log(Context.ConnectionId);
+
+            //see if the user was already in a group
+            RejoinGroup(Context.ConnectionId);
+
+            return base.OnConnected();
+        }
+
+        public override System.Threading.Tasks.Task OnDisconnected()
+        {
+            Log(Context.ConnectionId);
+            return base.OnDisconnected();
+        }
+
+        public override System.Threading.Tasks.Task OnReconnected()
+        {
+            Log(Context.ConnectionId);
+            return base.OnReconnected();
+        }
+
+        private void Log(string p)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RejoinGroup(string s)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ ComicTales.Web/[Code]/SignalR/HubAuthorizationAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace ComicTales.SignalR
+{
+    //public class HubAuthorizeAttribute : Attribute, IAuthorizeHubConnection, IAuthorizeHubMethodInvocation
+    //{
+    //    public virtual bool AuthorizeHubConnection(HubDescriptor hubDescriptor, Microsoft.AspNet.SignalR.IRequest request)
+    //    {
+    //        IAuthorizationProvider authorizationProvider = DependencyResolver.Current.GetService<IAuthorizationProvider>();
+
+    //        return authorizationProvider.IsAuthorizedController(hubDescriptor.Name);
+    //    }
+
+    //    public virtual bool AuthorizeHubMethodInvocation(IHubIncomingInvokerContext hubIncomingInvokerContext)
+    //    {
+    //        IAuthorizationProvider authorizationProvider = DependencyResolver.Current.GetService<IAuthorizationProvider>();
+
+    //        return authorizationProvider.IsAuthorizedAction(hubIncomingInvokerContext.MethodDescriptor.Hub.Name, hubIncomingInvokerContext.MethodDescriptor.Name);
+    //    }
+    //}
+}
